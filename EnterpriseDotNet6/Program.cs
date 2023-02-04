008df7a7b33bbf365a47606cea9e451475813238
John B. Roberts <johnbroberts@gmail.com>
2023-02-04 16:52:29 -0500

    JBR - Added JSON formatted log output

--- EnterpriseDotNet6/Program.cs
+++ EnterpriseDotNet6/Program.cs
@@ -1,8 +1,12 @@
+using EnterpriseDotNet6.API.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
 using Serilog;
+using Serilog.Formatting.Json;
 
 // Initialize Serilog Settings
 Log.Logger = new LoggerConfiguration()
-    .WriteTo.Console()
+    .WriteTo.Console(new JsonFormatter())
+    .MinimumLevel.Verbose()
     .CreateBootstrapLogger();
 
 Log.Information("Application Starting Up");
@@ -13,7 +17,13 @@ try
     // Create Builder
     var builder = WebApplication.CreateBuilder(args);
 
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .ReadFrom.Configuration(ctx.Configuration));
+
     // Add services to the container.
+
+    builder.Services.ConfigureCors();
+    builder.Services.ConfigureIISIntegration();
 
     builder.Services.AddControllers();
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -22,17 +32,36 @@ try
 
     var app = builder.Build();
 
+    app.UseSerilogRequestLogging();
+
     // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
     {
         app.UseSwagger();
         app.UseSwaggerUI();
+        Log.Fatal("Test Fatal");
+        Log.Error("Test Error");
+        Log.Warning("Test Warning");
+        Log.Information("Test Information");
+        Log.Debug("Test Debug");
+        Log.Verbose("Test Verbose");
     }
+    else
+        app.UseHsts();
 
     app.UseHttpsRedirection();
 
-    app.UseAuthorization();
+    app.UseStaticFiles();
 
+    _ = app.UseForwardedHeaders(new ForwardedHeadersOptions
+    {
+        ForwardedHeaders = ForwardedHeaders.All
+    });
+
+    app.UseCors("CorsPolicy");
+
+    app.UseAuthorization();
+    
     app.MapControllers();
 
     app.Run();
