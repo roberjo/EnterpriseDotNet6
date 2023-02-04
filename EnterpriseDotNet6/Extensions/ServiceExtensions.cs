aded5e3eb00deda02d9ab99af688cf2f35205bb1
John B. Roberts <johnbroberts@gmail.com>
2023-02-04 15:35:08 -0500

    JBR - Adding CORS service extensions

---
+++ EnterpriseDotNet6/Extensions/ServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace EnterpriseDotNet6.API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        }
+    }
+}
